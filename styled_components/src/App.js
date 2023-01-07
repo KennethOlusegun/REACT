bf650c5ffcef435aff1a6abd18b33491b69ee0ca
Kenneth Olusegun Dornelles de Jesus <kojesus@arezzo.com.br>
2023-01-07 11:53:47 -0300

    feat: styled components functions

---
+++ styled_components/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Title, TitleSmall } from "./styles";
+
+function App() {
+  return (
+    <div className="App">
+		  <Title fontSize={20}>Hello World
+			<span>texto menor</span>
+		  </Title>
+
+		<TitleSmall>Menor</TitleSmall>
+    </div>
+  );
+}
+
+export default App;
